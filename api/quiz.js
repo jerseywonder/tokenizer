0ae9a388a76ca32cd180a7e56993ab9d0a39b879
Andy Ball <work@andyball.info>
2024-01-10 09:52:17 +1100

    Add risk quiz api

--- api/quiz.js
+++ api/quiz.js
@@ -12,7 +12,7 @@ const query = async (sql, params = []) => pool.query(sql, params)
 
 async function postresults(values) {
   const sql = `
-  INSERT INTO worst (q1, q1j, q1r, q2, q2j, q2r, q3, q3j, q3r, q4, q4j, q4r, q5, q5j, q5r, q6, q6j, q6r, q7, q7j, q7r, q8, q8j, q8r, q9, q9j, q9r, q10, q10j, q10r) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30) returning id;`
+  INSERT INTO worse (q1, q1j, q1r, q2, q2j, q2r, q3, q3j, q3r, q4, q4j, q4r, q5, q5j, q5r, q6, q6j, q6r, q7, q7j, q7r, q8, q8j, q8r, q9, q9j, q9r, q10, q10j, q10r) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30) returning id;`
   const { rows } = await query(sql, values)
   return rows[0].id
 }
@@ -25,6 +25,7 @@ module.exports = async function quiz(req, res, next) {
 
         if (data.worst) {
 
+
             const worst = data.worst.map(d => {
                 return { a : d.correct, b : JSON.stringify(d.data),  c : d.rmse }
             })
@@ -36,6 +37,7 @@ module.exports = async function quiz(req, res, next) {
             let id = await postresults(flattenedArray)
 
             console.log(id)
+
 
         }
 
